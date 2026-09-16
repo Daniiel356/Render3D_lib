41187944fa2e116b17f12487a81454db4374af1d
Daniiel356 <255756069+Daniiel356@users.noreply.github.com>
2026-09-15 18:28:29 -0600

    update main.js

--- main.js
+++ main.js
@@ -4,4 +4,24 @@ const render=new Render();
 const canvas=render.getElement();
 
 document.body.appendChild(canvas);
-render.draw();
+const program=await render.createProgram("./lib/shaders/source/vertex_2D.glsl", "./lib/shaders/source/fragment_2D.glsl")
+
+render.addGeometry(
+    [
+        -0.5,  0.5,
+        -0.5, -0.5,
+         0.5, -0.5,
+         0.5, -0.5,
+         0.5,  0.5,
+        -0.5,  0.5
+    ],
+    [{
+        pos: 0,
+        size: 2,
+        type: "FLOAT",
+        normalize: false,
+        stride: 8,
+        offset: 0
+    }]
+)
+render.draw(program);
