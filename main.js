593b50c69f0f1fb7c1ab3eae31796b0c9b185a19
Daniiel356 <255756069+Daniiel356@users.noreply.github.com>
2026-09-16 11:44:34 -0600

    update main.js

--- main.js
+++ main.js
@@ -9,28 +9,28 @@ const program=await render.createProgram("./lib/shaders/source/vertex_2D.glsl", 
 
 render.addGeometry(
     [
-        -0.5,  0.5, 1, 0, 0, 1,
-        -0.5, -0.5, 1, 0, 0, 1,
-         0.5, -0.5, 1, 0, 0, 1,
-         0.5, -0.5, 1, 0, 0, 1,
-         0.5,  0.5, 1, 0, 0, 1,
-        -0.5,  0.5, 1, 0, 0, 1
+        -0.5,  0.5, 0, 1,
+        -0.5, -0.5, 0, 0,
+         0.5, -0.5, 1, 0,
+         0.5, -0.5, 1, 0,
+         0.5,  0.5, 1, 1,
+        -0.5,  0.5, 0, 1
     ],
     [{
         pos: 0,
         size: 2,
         type: "FLOAT",
         normalize: false,
-        stride: 24,
+        stride: 16,
         offset: 0
     },
      {
          pos: 1,
-         size: 4,
+         size: 2,
          type: "FLOAT",
          normalize: false,
          stride: 24,
-         offset: 8
+         offset: 16
      }]
 )
 render.draw(program);
