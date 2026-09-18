7ca21c15b2f975cce5d23285511d8a7f5a490321
Daniiel356 <255756069+Daniiel356@users.noreply.github.com>
2026-09-18 17:42:43 -0600

    update lib/shaders/source/vertex_2D.glsl

--- lib/shaders/source/vertex_2D.glsl
+++ lib/shaders/source/vertex_2D.glsl
@@ -2,13 +2,13 @@
 
 layout(location = 0) in vec2 a_base_pos;
 layout(location = 1) in vec2 a_pos;
-//layout(location = 2) in vec2 a_scale;
+layout(location = 2) in vec2 a_scale;
 //layout(location = 3) in vec2 a_uv;
 
 //out vec2 b_uv;
 
 void main(){
-    vec2 pos=a_base_pos+a_pos;//(a_base_pos * a_scale) + a_pos;
+    vec2 pos=(a_base_pos*a_scale)+a_pos;//(a_base_pos * a_scale) + a_pos;
     gl_Position=vec4(pos, 0.0, 1.0);
  //   b_uv=a_uv;
 }
