38ff3193714657e5e0ba56913f1a5c340759b3d4
Daniiel356 <255756069+Daniiel356@users.noreply.github.com>
2026-09-18 20:38:10 -0600

    update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import {Render} from "/lib/render.js";
-
+import {Material} from "./lib/engine/material.js";
+    
 const render=new Render();
 const canvas=render.element;
 
@@ -8,7 +9,6 @@ render.resize();
 const program=await render.createProgram("./lib/shaders/source/vertex_2D.glsl", "./lib/shaders/source/fragment_2D.glsl", 0)
 const textureManager=render.textureManager;
 
-const redTexture=textureManager.add(await textureManager.colorTexture({r:255}));
 const cube=render.createGeometry(
     [
         0, 0,
@@ -17,39 +17,12 @@ const cube=render.createGeometry(
         0, 0,
         1, 0,
         1, 1,
-    ],
-    [{
-        pos: 0,
-        size: 2,
-        type: "FLOAT",
-        normalize: false,
-        stride: 0,
-        offset: 0,
-        base: true
-    },{
-        pos: 1,
-        size: 2,
-        type: "FLOAT",
-        normalize: false,
-        stride: 32,
-        offset: 0,
-    },{
-         pos: 2,
-         size: 2,
-         type: "FLOAT",
-         normalize: false,
-         stride: 32,
-         offset: 8
-     },{
-         pos: 3,
-         size: 4,
-         type: "FLOAT",
-         normalize: false,
-         stride: 32,
-         offset: 16
-     }
-    ]
+    ], render.BASIC_GEOMETRY
 );
+const texture=textureManager.add(await textureManager.colorTexture({r:255}));
+const material=new Material(texture);
+
+
 await render.init();
 cube.addInstance([-0.25,-0.5, 0.5, 1, ...redTexture.uvs])
 
