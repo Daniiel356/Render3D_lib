99ca8575222420825a54069cd51510cef9d1cfee
Daniiel356 <255756069+Daniiel356@users.noreply.github.com>
2026-09-18 19:21:31 -0600

    update lib/shaders/source/fragment_2D.glsl

--- lib/shaders/source/fragment_2D.glsl
+++ lib/shaders/source/fragment_2D.glsl
@@ -1,11 +1,11 @@
 #version 300 es
 precision mediump float;
 
-//uniform sampler2D u_texture;
+uniform sampler2D u_texture;
 
-//in vec2 b_uv;
+in vec2 b_uv;
 out vec4 out_Color;
 
 void main(){
-    out_Color=vec4(1,0,0,1);//texture(u_texture, b_uv);
+    texture(u_texture, b_uv);
 }
